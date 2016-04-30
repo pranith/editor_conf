ee2a338b96a94f3bf6be823396129c6847d392ac
Pranith Kumar <bobby.prani@gmail.com>
2016-04-30 13:46:11 -0400

    Update do setup script

--- do_setup.sh
+++ do_setup.sh
@@ -3,9 +3,9 @@
 apt update
 apt -y install vim emacs git byobu gcc g++
 addgroup pranith
-useradd pranith --create-home --shell /bin/bash --groups pranith
+useradd pranith --create-home --shell /bin/bash -g pranith
 mkdir -p /home/pranith/.ssh
 chmod 700 /home/pranith/.ssh
-cat .ssh/authorized_keys >> /home/pranith/.ssh/authorized_keys
+cat ~/.ssh/authorized_keys >> /home/pranith/.ssh/authorized_keys
 cp -rf ~/editor_conf /home/pranith/
 chown pranith:pranith /home/pranith -R
